eb4cd21fbf012c469f975745b55f5626ef0a3922
Sergey Alyaev <sergey@al-digital.no>
2020-10-26 08:48:10 +0100

    Added sounds tests

--- examples/duplo_train_basics.js
+++ examples/duplo_train_basics.js
@@ -1,5 +1,4 @@
-const { connected } = require("process");
-const PoweredUP = require("node-poweredup");
+const PoweredUP = require("..");
 const poweredUP = new PoweredUP.PoweredUP();
 
 poweredUP.on("discover", async (hub) => { // Wait to discover a Hub
@@ -20,21 +19,46 @@ poweredUP.on("discover", async (hub) => { // Wait to discover a Hub
         motor.setPower(100);
         const color = await hub.waitForDeviceAtPort("COLOR"); 
         console.log("Connected color");
-        color.on("rgb", (rgb) => {
-            console.log(`Color ${rgb.red}, ${rgb.green}, ${rgb.blue}`);
-        });
+        // color.on("rgb", (rgb) => {
+        //     console.log(`Color ${rgb.red}, ${rgb.green}, ${rgb.blue}`);
+        // });
         const speed = await hub.waitForDeviceAtPort("SPEEDOMETER"); 
         console.log("Connected SPEEDOMETER");
-        speed.on("speed", (speed) => {
-            console.log(`Speed ${speed.speed}`);
-        });
+        // speed.on("speed", (speed) => {
+        //     console.log(`Speed ${speed.speed}`);
+        // });
 
         const led = await hub.waitForDeviceByType(PoweredUP.Consts.DeviceType.HUB_LED);
-        console.log("Connected led");
+        console.log(`Connected led at port ${led.portName}`);
         led.setRGB(0,255,0);
 
         const speaker = await hub.waitForDeviceByType(PoweredUP.Consts.DeviceType.DUPLO_TRAIN_BASE_SPEAKER);
-        console.log("Connected speaker");
+        console.log(`Connected speaker at port ${speaker.portName}`);
+        const legalSounds = [3, 5, 7, 9, 10];
+
+        // for (let i = 0; i<legalSounds.length; ++i){
+        //     console.log(`playing ${legalSounds[i]}`);
+        //     await speaker.playSound(legalSounds[i]);
+        //     await hub.sleep(2000);
+        // }
+
+        const legalTones = [1, 2, 3, 5, 7, 9, 10]; 
+        for (let i = 0; i<16; ++i){
+            console.log(`playing tone ${i}`);
+            await speaker.playTone(i);
+            await hub.sleep(5000);
+        }
+
+        console.log(`playing STATION DEPARTURE`);
+
+        await speaker.playSound(PoweredUP.Consts.DuploTrainBaseSound.STATION_DEPARTURE);
+        await hub.sleep(2000);
+
+        console.log(`playing STEAM`);
+
+        await speaker.playSound(PoweredUP.Consts.DuploTrainBaseSound.STEAM);
+        await hub.sleep(2000);
+
 
     }
 
